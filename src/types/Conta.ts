80dd1f1359dd3c225ed99b8d17fdb23782e9badc
GuirraMv <matheusguirravieira15@gmail.com>
2023-10-02 14:42:35 -0300

    style: formatting code with prettier

--- src/types/Conta.ts
+++ src/types/Conta.ts
@@ -1,101 +1,96 @@
-import { Armazenador } from "./Armazenador.js";
-import { ValidaDebito, validaDeposito } from "./Decorators.js";
-import { GrupoTransacao } from "./GrupoTransacao.js";
-import { TipoTransacao } from "./TipoTransacao.js";
-import { Transacao } from "./Transacao.js";
+import { Armazenador } from './Armazenador.js'
+import { ValidaDebito, validaDeposito } from './Decorators.js'
+import { GrupoTransacao } from './GrupoTransacao.js'
+import { TipoTransacao } from './TipoTransacao.js'
+import { Transacao } from './Transacao.js'
 
 export class Conta {
-  protected nome: string;
-  protected saldo: number = Armazenador.obter<number>("saldo") || 0;
+  protected nome: string
+  protected saldo: number = Armazenador.obter<number>('saldo') || 0
   private transacoes: Transacao[] =
-    Armazenador.obter<Transacao[]>("transacoes", (key: string, value: any) => {
-      if (key === "data") {
-        return new Date(value);
+    Armazenador.obter<Transacao[]>('transacoes', (key: string, value: any) => {
+      if (key === 'data') {
+        return new Date(value)
       }
-      return value;
-    }) || [];
+      return value
+    }) || []
 
   constructor(nome: string) {
-    this.nome = nome;
+    this.nome = nome
   }
   public getTitular() {
-    return this.nome;
+    return this.nome
   }
   getGruposTransacoes(): GrupoTransacao[] {
-    const gruposTransacoes: GrupoTransacao[] = [];
-    const listaTransacoes: Transacao[] = structuredClone(this.transacoes);
-    const transacoesOrdenadas: Transacao[] = listaTransacoes.sort(
-      (t1, t2) => t2.data.getTime() - t1.data.getTime()
-    );
-    let labelAtualGrupoTransacao: string = "";
+    const gruposTransacoes: GrupoTransacao[] = []
+    const listaTransacoes: Transacao[] = structuredClone(this.transacoes)
+    const transacoesOrdenadas: Transacao[] = listaTransacoes.sort((t1, t2) => t2.data.getTime() - t1.data.getTime())
+    let labelAtualGrupoTransacao: string = ''
 
     for (let transacao of transacoesOrdenadas) {
-      let labelGrupoTransacao: string = transacao.data.toLocaleDateString(
-        "pt-br",
-        { month: "long", year: "numeric" }
-      );
+      let labelGrupoTransacao: string = transacao.data.toLocaleDateString('pt-br', { month: 'long', year: 'numeric' })
       if (labelAtualGrupoTransacao !== labelGrupoTransacao) {
-        labelAtualGrupoTransacao = labelGrupoTransacao;
+        labelAtualGrupoTransacao = labelGrupoTransacao
         gruposTransacoes.push({
           label: labelGrupoTransacao,
           transacoes: [],
-        });
+        })
       }
-      gruposTransacoes.at(-1).transacoes.push(transacao);
+      gruposTransacoes.at(-1).transacoes.push(transacao)
     }
 
-    return gruposTransacoes;
+    return gruposTransacoes
   }
 
   getSaldo() {
-    return this.saldo;
+    return this.saldo
   }
   getDataAcesso(): Date {
-    return new Date();
+    return new Date()
   }
   registrarTransacao(novaTransacao: Transacao): void {
     if (novaTransacao.tipoTransacao == TipoTransacao.DEPOSITO) {
-      this.depositar(novaTransacao.valor);
+      this.depositar(novaTransacao.valor)
     } else if (
       novaTransacao.tipoTransacao == TipoTransacao.TRANSFERENCIA ||
       novaTransacao.tipoTransacao == TipoTransacao.PAGAMENTO_BOLETO
     ) {
-      this.debitar(novaTransacao.valor);
-      novaTransacao.valor *= -1;
+      this.debitar(novaTransacao.valor)
+      novaTransacao.valor *= -1
     } else {
-      throw new Error("Tipo de Transação é inválido!");
+      throw new Error('Tipo de Transação é inválido!')
     }
 
-    this.transacoes.push(novaTransacao);
-    console.log(this.getGruposTransacoes());
-    Armazenador.salvar("transacoes", JSON.stringify(this.transacoes));
+    this.transacoes.push(novaTransacao)
+    console.log(this.getGruposTransacoes())
+    Armazenador.salvar('transacoes', JSON.stringify(this.transacoes))
   }
 
   @ValidaDebito
   debitar(valor: number): void {
-    this.saldo -= valor;
-    Armazenador.salvar("saldo", this.saldo.toString());
+    this.saldo -= valor
+    Armazenador.salvar('saldo', this.saldo.toString())
   }
 
   @validaDeposito
   depositar(valor: number): void {
-    this.saldo += valor;
-    Armazenador.salvar("saldo", this.saldo.toString());
+    this.saldo += valor
+    Armazenador.salvar('saldo', this.saldo.toString())
   }
 }
 
 export class ContaPremium extends Conta {
   registrarTransacao(Transacao: Transacao): void {
     if (Transacao.tipoTransacao === TipoTransacao.DEPOSITO) {
-      console.log("Ganhou um bônus");
-      Transacao.valor += 0.5;
+      console.log('Ganhou um bônus')
+      Transacao.valor += 0.5
     }
-    super.registrarTransacao(Transacao);
+    super.registrarTransacao(Transacao)
   }
 }
 
-const conta = new Conta("Joana da Silva Olveira");
+const conta = new Conta('Joana da Silva Olveira')
 
-const contaPremium = new ContaPremium("Japoro");
+const contaPremium = new ContaPremium('Japoro')
 
-export default conta;
+export default conta
--- src/types/Decorators.ts
+++ src/types/Decorators.ts
@@ -2,39 +2,29 @@
 //propertykey é o nome do método que está, no target será armazenado todo o método e o propertykey recebe apenas a sua chave e seu nome
 // desciptor é um objeto do tipo PropertyDescriptor, que terá uma informação sobre o método como seu value.
 
-export function ValidaDebito(
-  target: any,
-  propertyKey: string,
-  descriptor: PropertyDescriptor
-) {
-  const originalMethod = descriptor.value;
+export function ValidaDebito(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value
 
   //validação
   descriptor.value = function (valorDoDebito: number) {
     if (valorDoDebito <= 0) {
-      throw new Error("O valor a ser debitado precisa ser maior do que zero!");
+      throw new Error('O valor a ser debitado precisa ser maior do que zero!')
     }
     if (valorDoDebito > this.saldo) {
-      throw new Error("Seu saldo é insuficiente!");
+      throw new Error('Seu saldo é insuficiente!')
     }
-    return originalMethod.apply(this, [valorDoDebito]);
-  };
-  return descriptor;
+    return originalMethod.apply(this, [valorDoDebito])
+  }
+  return descriptor
 }
 
-export function validaDeposito(
-  target: any,
-  propertyKey: string,
-  descriptor: PropertyDescriptor
-) {
-  const originalMethod = descriptor.value;
+export function validaDeposito(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value
   descriptor.value = function (valorDoDeposito: number) {
     if (valorDoDeposito <= 0) {
-      throw new Error(
-        "O valor a ser depositado precisa ser maior do que zero!"
-      );
+      throw new Error('O valor a ser depositado precisa ser maior do que zero!')
     }
-    return originalMethod.apply(this, [valorDoDeposito]);
-  };
-  return descriptor;
+    return originalMethod.apply(this, [valorDoDeposito])
+  }
+  return descriptor
 }
